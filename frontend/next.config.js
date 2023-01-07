4d7af95202bd97233232bb51e351d3f6e5cafffe
PeakVisionMediaKLG <michael@peakvision.ch>
2023-01-07 21:32:00 +0100

    Fix for unresponsive docker container states

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    },})
 }
 
 module.exports = nextConfig
